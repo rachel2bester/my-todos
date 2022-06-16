6f8625a4a33bffa571c2486ba2ef0a2569dc72d8
rachel2bester <rachel2bester@gmail.com>
2022-06-16 12:33:21 +0200

    trying to sort checkboxes

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Task.scss"
 import trash from "./../../assets/images/trash-can-solid.svg"
 
 const Task = (props) => {
     const {title, onDelete, id} = props
+
+    const [done, setDone] = useState(false)
+
+    const toggleDone = () => {
+        setDone(!done)
+    }
+
     console.log(id)
     return (
         <div className="task">
-            <p className='task__title'>{title}</p>
+            <input className='' onChange={toggleDone} type="checkbox"/>
+            {done ? <p className='task__title done' >{title}</p> : <p className='task__title'>{title}</p> }
+            
             <img  onClick={onDelete} id={id} className='task__button' src={trash} alt="delete" />
         </div>
     )
